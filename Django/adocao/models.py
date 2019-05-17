8b1443c8fb9f67327a5d10d90a92c67cabf8042b
Aluno <>
2019-05-17 20:55:57 -0300

    Ativando Django

--- Django/adocao/models.py
+++ Django/adocao/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Estado(models.Model):
+    # nome_do_atributo = models.Tipo(configuração, exemplo:quatidade)
+    
+    sigla   = models.CharField(max_length=2)
+    nome    = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sigla + " - " + self.nome
+
+class Cidade(models.Model):
+    nome        = models.CharField(max_length=50)
+    estado      = models.ForeignKey(Estado, on_delete=models.PROTECT)
+    descricao   = models.TextField( blank=True, null=True, verbose_name="Descrição", help_text="Espaço para informações adicionais.")
+
+    def __str__(self):
+        return self.nome + " - " + self.estado.sigla
+
+class Pessoa(models.Model):
+
+    nome        = models.CharField(max_length=50, verbose_name="Qual seu nome?", help_text="Digite seu nome completo")
+    nascimento  = models.DateField(verbose_name='data de nascimento')
+    email       = models.CharField(max_length=100)
+    tefelone    = models.CharField(max_length=20)
+
+    cidade      = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome + ' - ' + str(self.nascimento)
