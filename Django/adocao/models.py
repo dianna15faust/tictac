2477c3b0c9bb5184068ac749f1d31dd1b5ff891f
dianna15faust <faustdianna.df@gmail.com>
2019-08-20 16:29:21 -0300

    finalizando cadastros

--- Django/adocao/models.py
+++ Django/adocao/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Estado(models.Model):
     # nome_do_atributo = models.Tipo(configuração, exemplo:quatidade)
-    
+
     sigla   = models.CharField(max_length=2)
     nome    = models.CharField(max_length=50)
 
@@ -25,9 +25,45 @@ class Pessoa(models.Model):
     nascimento  = models.DateField(verbose_name='data de nascimento')
     email       = models.CharField(max_length=100)
     telefone    = models.CharField(max_length=20, help_text="(DD) XXXX-XXXX")
-    endereco   = models.CharField(max_length=100)
+    endereco    = models.CharField(max_length=100)
 
     cidade      = models.ForeignKey(Cidade, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome + ' - ' + str(self.nascimento)
+
+class Admin(models.Model):
+
+    cpf                = models.CharField(max_length=11)
+    carteira_trabalho  = models.CharField(max_length=100)
+
+    pessoa             = models.ForeignKey(Pessoa, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.pessoa.nome + ' - ' + str(self.cpf)
+
+class Leitor(models.Model):
+
+    cartao_credito   = models.CharField(max_length=50)
+
+    pessoa           = models.ForeignKey(Pessoa, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.pessoa.nome + ' - ' + str(self.cartao_credito)
+
+class Acervo(models.Model):
+
+    titulo        = models.CharField(max_length=50, help_text="Digite seu titulo da obra completo")
+    genero       = models.CharField(max_length=100)
+    descricao    = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome + ' - ' + str(self.nascimento)
+class Pessoa(models.Model):
+
+    nome        = models.CharField(max_length=50, verbose_name="Qual seu nome?", help_text="Digite seu nome completo")
+    genero       = models.CharField(max_length=100)
+    descricao    = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome + ' - ' + str(self.genero)
